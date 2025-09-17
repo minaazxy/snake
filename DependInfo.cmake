126e65bb8acdf1821e6e883ac7de489c8e900fd7
Mina <mina.zhan1027@gmail.com>
2025-09-17 12:38:15 +0800

    Add files via upload

---
+++ DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/fangke/Downloads/HW1_Sprouthon_Mac/src/apple.cpp" "CMakeFiles/Sprouthon.dir/src/apple.cpp.o" "gcc" "CMakeFiles/Sprouthon.dir/src/apple.cpp.o.d"
+  "/Users/fangke/Downloads/HW1_Sprouthon_Mac/src/game.cpp" "CMakeFiles/Sprouthon.dir/src/game.cpp.o" "gcc" "CMakeFiles/Sprouthon.dir/src/game.cpp.o.d"
+  "/Users/fangke/Downloads/HW1_Sprouthon_Mac/src/grid.cpp" "CMakeFiles/Sprouthon.dir/src/grid.cpp.o" "gcc" "CMakeFiles/Sprouthon.dir/src/grid.cpp.o.d"
+  "/Users/fangke/Downloads/HW1_Sprouthon_Mac/src/main.cpp" "CMakeFiles/Sprouthon.dir/src/main.cpp.o" "gcc" "CMakeFiles/Sprouthon.dir/src/main.cpp.o.d"
+  "/Users/fangke/Downloads/HW1_Sprouthon_Mac/src/python.cpp" "CMakeFiles/Sprouthon.dir/src/python.cpp.o" "gcc" "CMakeFiles/Sprouthon.dir/src/python.cpp.o.d"
+  "/Users/fangke/Downloads/HW1_Sprouthon_Mac/src/scoreboard.cpp" "CMakeFiles/Sprouthon.dir/src/scoreboard.cpp.o" "gcc" "CMakeFiles/Sprouthon.dir/src/scoreboard.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sprouthon.dir/src/apple.cpp.o"
+  "CMakeFiles/Sprouthon.dir/src/apple.cpp.o.d"
+  "CMakeFiles/Sprouthon.dir/src/game.cpp.o"
+  "CMakeFiles/Sprouthon.dir/src/game.cpp.o.d"
+  "CMakeFiles/Sprouthon.dir/src/grid.cpp.o"
+  "CMakeFiles/Sprouthon.dir/src/grid.cpp.o.d"
+  "CMakeFiles/Sprouthon.dir/src/main.cpp.o"
+  "CMakeFiles/Sprouthon.dir/src/main.cpp.o.d"
+  "CMakeFiles/Sprouthon.dir/src/python.cpp.o"
+  "CMakeFiles/Sprouthon.dir/src/python.cpp.o.d"
+  "CMakeFiles/Sprouthon.dir/src/scoreboard.cpp.o"
+  "CMakeFiles/Sprouthon.dir/src/scoreboard.cpp.o.d"
+  "Sprouthon"
+  "Sprouthon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sprouthon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
